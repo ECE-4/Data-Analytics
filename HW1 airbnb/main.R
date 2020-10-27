aeb367c2c0fe541e3a5e9b9b8a0825fd807154ab
NeilSCGH <neil.segard@gmail.com>
2020-10-27 17:48:23 +0100

    read a csv

--- HW1 airbnb/main.R
+++ HW1 airbnb/main.R
@@ -1,3 +1,6 @@
-malagaCalendar <- read.csv(file = 'data/malaga/calendar.csv')
-head(malagaCalendar)
+malagaCal <- read.csv(file = 'data/malaga/calendar.csv')
+head(malagaCal)
+#head(malagaCalendar["date"])
 
+malagaList <- read.csv(file = 'data/malaga/listings.csv')
+names(malagaList) #affiche le nom des colonnes
