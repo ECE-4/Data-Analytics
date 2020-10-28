22644a7a70fcc53043d4efdddfe4662db853173c
NeilSCGH <neil.segard@gmail.com>
2020-10-28 16:36:12 +0100

    q1

--- HW1 airbnb/main.R
+++ HW1 airbnb/main.R
@@ -1,6 +1,36 @@
-malagaCal <- read.csv(file = 'data/malaga/calendar.csv')
-head(malagaCal)
-#head(malagaCalendar["date"])
+#Get the data
+#malagaCal <- read.csv(file = 'data/malaga/calendar.csv')
+#mallorcaCal <- read.csv(file = 'data/mallorca/calendar.csv')
+#sevillaCal <- read.csv(file = 'data/sevilla/calendar.csv')
 
-malagaList <- read.csv(file = 'data/malaga/listings.csv')
-names(malagaList) #affiche le nom des colonnes
+#Add the city
+malagaCal$city="malaga"
+mallorcaCal$city="mallorca"
+sevillaCal$city="sevilla"
+
+#concatenate all the data
+calendars <- rbind(malagaCal, mallorcaCal, sevillaCal)
+
+#preprocess the availability
+calendars$available[calendars$available=="t"] <- 1
+calendars$available[calendars$available=="f"] <- 0
+calendars$available <- as.numeric(calendars$available)
+
+#Compute the availability over 30 days
+avgAvailability <- aggregate(calendars$available, by=list(Category=calendars$city), FUN=mean)
+names(avgAvailability) <- c("listing_id","availability")
+avgAvailability$availability <- avgAvailability$availability * 30
+
+#Point 1&2
+head(avgAvailability)
+
+#Point 3&4
+
+#Point 5
+
+#Point 6
+
+#Point 7
+
+#Point 8
+
